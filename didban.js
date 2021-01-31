0ea2357176b40490822a150d37aa71039861fee8
Kaveh RezaeiShiraz <kavehrs@pm.me>
2021-01-31 08:39:14 +0330

    Update didban.js

--- didban.js
+++ didban.js
@@ -7,7 +7,7 @@
  FUNCTION:  Get Data From Cleint side and send for IRIB Analytic Server
  INPUT:     ACTIVITY, SERVICE_TYPE, CONTENT_TYPE
  ****************************************************************/
-var url = "http://192.168.143.18:8876/api/",
+var url = "https://statistics.irib.ir:8876/api/",
     auth_token = "Bearer DE9C3CFBF147067970C4CAC7F3874247", sys_id = "ُiribcsspr99", system_id;
 var user_id, active_session, ip, session_id, ttl = 30, ttl = 30, counter = ttl;
 
