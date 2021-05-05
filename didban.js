fd11e25e9e98c17ab0b144ad1eb83455c3140b98
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-05-05 16:51:42 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -131,12 +131,22 @@ function setCookie(key, value) {
 }
 
 
+// function create_UUID() {
+//     var e = (new Date).getTime();
+//     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (t) {
+//         var n = (e + 16 * Math.random()) % 16 | 0;
+//         return e = Math.floor(e / 16), ("x" == t ? n : 3 & n | 8).toString(16)
+//     })
+// }
+
 function create_UUID() {
-    var e = (new Date).getTime();
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (t) {
-        var n = (e + 16 * Math.random()) % 16 | 0;
-        return e = Math.floor(e / 16), ("x" == t ? n : 3 & n | 8).toString(16)
-    })
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
 }
 
 String.prototype.format || (String.prototype.format = function () {
