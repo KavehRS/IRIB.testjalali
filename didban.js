1a19f8d6f39e8968bb34d46e4a5fc66358366b51
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-05-08 09:59:37 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -126,7 +126,7 @@ function setCookie(key, value) {
     }
 
     var dt = new Date();
-    dt.setHours(dt.getHoures() + timeout);
+    dt.setMinutes(dt.getMinutes() + 5*timeout);
     document.cookie = "{0}={1}; expires={2}".format(key, value, dt.toUTCString());
 }
 
