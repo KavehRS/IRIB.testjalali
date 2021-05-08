659fe7286362cd2363da55c9803863166a934edf
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-05-08 10:34:36 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -188,43 +188,39 @@ String.prototype.format || (String.prototype.format = function () {
     })
 }),
     
- getUserIP(function (e) {
+getUserIP(function (e) {
     ip = e
 }), sessionFactory = {
     check: function () {
-        var e = getCookie("token1");
+        var e = getCookie("token");
         return e ? (active_session = e, console.log("Session is already opened. Token {0}".format(e))) : sessionFactory.init(session_id), !0
     }, init: function (e) {
         if (ip) {
             var t = getCookie("token");
-            var m = getCookie("token1");
-            if (session_id != e || !m) {
-                t = create_UUID();
-                m = create_SID();
-                user_id = null != e ? e : t, setCookie("token", t), session_id = null != e ? m : m,  _setCookie("token1", m), user_agent = navigator.userAgent, referer = document.location.origin, xReferer = document.location.origin;
-                var n = '{"sys_id": "{0}", "user_id": "{1}", "session_id": "{2}", "ip": "{3}","user_agent": "{4}", "referer": "{5}", "xReferer": "{6}"}'.format(system_id, user_id, session_id, ip, user_agent, referer, xReferer),
+            if (user_id != e || !t) {
+                t = create_SID();
+                session_id = null != e ? e : t, setCookie("token", t), user_agent = navigator.userAgent, referer = document.location.origin, xReferer = document.location.origin;
+                var n = '{"sys_id": "{0}", "user_id": "{1}", "session_id": "{2}", "ip": "{3}","user_agent": "{4}", "referer": "{5}", "xReferer": "{6}"}'.format(system_id, t, session_id, ip, user_agent, referer, xReferer),
                     o = new XMLHttpRequest;
                 return o.open("POST", "{0}session/".format(url), !0), o.setRequestHeader("Content-Type", "application/json"), o.setRequestHeader("Authorization", auth_token), o.onreadystatechange = function () {
                     4 == this.readyState && 201 == this.status ? console.log("Success: {0}: {1}".format(this.status, this.responseText)) : console.log("Error: {0}: {1}".format(this.status, this.responseText))
                 }, o.send(n), !0
             }
             setCookie("token", t)
-            _setCookie("token1",m)
         } else setTimeout(function () {
             0 != counter-- ? sessionFactory.init(session_id) : counter = ttl
         }, 1e3)
-    }, 
-    expire: function () {
-        return setCookie("token1", null), session_id = null, !0
+    }, expire: function () {
+        return setCookie("token", null), session_id = null, !0
     }
 }, activityFactory = {
     log: function (e, t, n, o, i, r) {
         sessionFactory.check();
-        var a = getCookie("token1"),
+        var a = getCookie("token"),
             s = '{"session_id": "{0}", "channel_id": "{1}", "content_id": "{2}","content_type_id": "{3}", "service_id": "{4}","action_id": "{5}", "time_code": "{6}"}'.format(a, e, t, n, o, i, r),
             c = new XMLHttpRequest;
         return c.open("POST", "{0}event/".format(url), !0), c.setRequestHeader("Content-Type", "application/json"), c.setRequestHeader("Authorization", auth_token), c.onreadystatechange = function () {
-            4 == this.readyState && 201 == this.status ? (_setCookie("token1", a), console.log("Token {0} did activity {1}".format(a, i))) : console.log("Activity logging failed.")
+            4 == this.readyState && 201 == this.status ? (setCookie("token", a), console.log("Token {0} did activity {1}".format(a, i))) : console.log("Activity logging failed.")
         }, c.send(s), !0
     }
 };
