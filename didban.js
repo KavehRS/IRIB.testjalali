d7cd02232317f0a2d1c9bde7842d036ac483ee6e
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-05-10 12:14:44 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -64,32 +64,6 @@ if (!String.prototype.format) {
 
 
 
-
-// A helper function for string manipulation
-// if (!String.prototype.format) {
-//     String.prototype.format = function () {
-//         var args = arguments;
-//         return this.replace(/{(\d+)}/g, function (match, number) {
-//             return typeof args[number] != 'undefined'
-//                 ? args[number]
-//                 : match
-//                 ;
-//         });
-//     };
-// }
-
-
-
-
-// function getCookie(name) {
-//     name += "=";
-//     for (var ca = decodeURIComponent(document.cookie).split(";"), n = 0; n < ca.length; n++) {
-//         for (var o = ca[n]; " " == o.charAt(0);) o = o.substring(1);
-//         if (0 == o.indexOf(name)) return o.substring(name.length, o.length)
-//     }
-//     return ""
-// }
-
 function getCookie(name) {
     name = name + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
@@ -108,14 +82,7 @@ function getCookie(name) {
 
 
 
-// function setCookie(key, value) {
-//     if (value) {
-//         var n = new Date;
-//         n.setMinutes(n.getMinutes() + timeout), document.cookie = "{0}={1}; expires={2}".format(key, value, n.toUTCString())
-//     } else document.cookie = "{0}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;".format(key)
-// }
-
-function setCookie(key, value) {
+function setCookie(key, value, _t) {
     if (!value) {
         // Expire cookie
         document.cookie = "{0}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;".format(key);
@@ -123,7 +90,7 @@ function setCookie(key, value) {
     }
 
     var dt = new Date();
-    dt.setMinutes(dt.getMinutes() + 1*timeout);
+    dt.setMinutes(dt.getMinutes() + _t*timeout);
     document.cookie = "{0}={1}; expires={2}".format(key, value, dt.toUTCString());
 }
 
@@ -167,20 +134,7 @@ function create_SID() {
     });
     return sid;
 }
-// String.prototype.format || (String.prototype.format = function () {
-//     var e = arguments;
-//     return this.replace(/{(\d+)}/g, function (t, n) {
-//         return void 0 !== e[n] ? e[n] : t
-//     })
-// }),
 
-// function create_UUID() {
-//     var e = (new Date).getTime();
-//     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (t) {
-//         var n = (e + 16 * Math.random()) % 16 | 0;
-//         return e = Math.floor(e / 16), ("x" == t ? n : 3 & n | 8).toString(16)
-//     })
-// }
 
 function create_UUID() {
     var dt = new Date().getTime();
@@ -213,9 +167,6 @@ String.prototype.format || (String.prototype.format = function () {
             var m = getCookie("sid");
             var x = getCookie("xid");
             if (user_id != e || !t) {
-//                 flag=1
-                
-
                                 
                 m = create_SID();
                 t = create_UUID();
@@ -224,55 +175,40 @@ String.prototype.format || (String.prototype.format = function () {
                 
                 if (x==""){
                     x = create_UUID();
-                    __setCookie("xid", x);
+                    setCookie("xid", x, 5);
                 }
                 
                 var x = getCookie("xid");
                 
-                user_id = null != e ? e : t, setCookie("uid", t), session_id = null != e ? m : m,  _setCookie("sid", m), user_agent = navigator.userAgent, referer = document.location.origin, xReferer = document.location.origin;
+                user_id = null != e ? e : t, setCookie("uid", t, 1), session_id = null != e ? m : m,  setCookie("sid", m, 1), user_agent = navigator.userAgent, referer = document.location.origin, xReferer = document.location.origin;
                 var n = '{"sys_id": "{0}", "user_id": "{1}", "session_id": "{2}", "ip": "{3}","user_agent": "{4}", "referer": "{5}", "xReferer": "{6}"}'.format(system_id, x , session_id, ip, user_agent, referer, xReferer),
                     o = new XMLHttpRequest;
                 return o.open("POST", "{0}session/".format(url), !0), o.setRequestHeader("Content-Type", "application/json"), o.setRequestHeader("Authorization", auth_token), o.onreadystatechange = function () {
                     4 == this.readyState && 201 == this.status ? console.log("Success: {0}: {1}".format(this.status, this.responseText)) : console.log("Error: {0}: {1}".format(this.status, this.responseText))
                 }, o.send(n), !0
             }
-            
-            
-//             if (flag != 1){
-//                 m = create_SID();
-//                 session_id = null != e ? m : m,  _setCookie("sid", m), user_agent = navigator.userAgent, referer = document.location.origin, xReferer = document.location.origin;
-//                 var n = '{"sys_id": "{0}", "user_id": "{1}", "session_id": "{2}", "ip": "{3}","user_agent": "{4}", "referer": "{5}", "xReferer": "{6}"}'.format(system_id, t, session_id, ip, user_agent, referer, xReferer),
-//                     o = new XMLHttpRequest;
-//                 return o.open("POST", "{0}session/".format(url), !0), o.setRequestHeader("Content-Type", "application/json"), o.setRequestHeader("Authorization", auth_token), o.onreadystatechange = function () {
-//                     4 == this.readyState && 201 == this.status ? console.log("Success: {0}: {1}".format(this.status, this.responseText)) : console.log("Error: {0}: {1}".format(this.status, this.responseText))
-//                 }, o.send(n), !0
-//             }          
-            
-//             flag = 0
+
             
             
             
-            setCookie("uid", t)
-            _setCookie("sid",m)
+            setCookie("uid", t, 1)
+            setCookie("sid",m, 1)
         } else setTimeout(function () {
             0 != counter-- ? sessionFactory.init(user_id) : counter = ttl
         }, 1e3)
     },
     expire: function () {
-        return setCookie("uid", null), user_id = null, !0
+        return setCookie("uid", null, 1), user_id = null, !0
     }
 }, activityFactory = {
     log: function (e, t, n, o, i, r) {
         sessionFactory.check();
         var a = getCookie("sid");
-//         if (a == null){
-//             a = create_SID();
-//             _setCookie("sid",a)
-//         }
+
         var s = '{"session_id": "{0}", "channel_id": "{1}", "content_id": "{2}","content_type_id": "{3}", "service_id": "{4}","action_id": "{5}", "time_code": "{6}"}'.format(a, e, t, n, o, i, r);
         var   c = new XMLHttpRequest;
         return c.open("POST", "{0}event/".format(url), !0), c.setRequestHeader("Content-Type", "application/json"), c.setRequestHeader("Authorization", auth_token), c.onreadystatechange = function () {
-            4 == this.readyState && 201 == this.status ? (_setCookie("sid", a), console.log("Token {0} did activity {1}".format(a, i))) : console.log("Activity logging failed.")
+            4 == this.readyState && 201 == this.status ? (setCookie("sid", a, 1), console.log("Token {0} did activity {1}".format(a, i))) : console.log("Activity logging failed.")
         }, c.send(s), !0
     }
 };
