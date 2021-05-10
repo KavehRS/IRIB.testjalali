08a9bfa9ca5ba1faabcffab637fe190c755d422d
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-05-10 14:40:33 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -50,17 +50,17 @@ function getUserIP(onNewIP) {
 
 
 // A helper function for string manipulation
-if (!String.prototype.format) {
-    String.prototype.format = function () {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function (match, number) {
-            return typeof args[number] != 'undefined'
-                ? args[number]
-                : match
-                ;
-        });
-    };
-}
+// if (!String.prototype.format) {
+//     String.prototype.format = function () {
+//         var args = arguments;
+//         return this.replace(/{(\d+)}/g, function (match, number) {
+//             return typeof args[number] != 'undefined'
+//                 ? args[number]
+//                 : match
+//                 ;
+//         });
+//     };
+// }
 
 
 
