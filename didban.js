368e469c3a54bb61ff19c0054b9f2e1194bbef17
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-05-18 11:20:53 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -49,29 +49,80 @@ function getUserIP(onNewIP) {
 }
 
 
-function ip_local()
-{
- var ipv = false;
- window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection || false;
+function getIPs(callback){
+    var ip_dups = {};
 
- if (window.RTCPeerConnection)
- {
-  ip = [];
-  var pc = new RTCPeerConnection({iceServers:[]}), noop = function(){};
-  pc.createDataChannel('');
-  pc.createOffer(pc.setLocalDescription.bind(pc), noop);
+    //compatibility for firefox and chrome
+    var RTCPeerConnection = window.RTCPeerConnection
+        || window.mozRTCPeerConnection
+        || window.webkitRTCPeerConnection;
+    var useWebKit = !!window.webkitRTCPeerConnection;
 
-  pc.onicecandidate = function(event)
-  {
-   if (event && event.candidate && event.candidate.candidate)
-   {
-    var s = event.candidate.candidate.split('\n');
-    ip.push(s[0].split(' ')[4]);
-   }
-  }
- }
+    //bypass naive webrtc blocking using an iframe
+    if(!RTCPeerConnection){
+        //NOTE: you need to have an iframe in the page right above the script tag
+        //
+        //<iframe id="iframe" sandbox="allow-same-origin" style="display: none"></iframe>
+        //<script>...getIPs called in here...
+        //
+        var win = iframe.contentWindow;
+        RTCPeerConnection = win.RTCPeerConnection
+            || win.mozRTCPeerConnection
+            || win.webkitRTCPeerConnection;
+        useWebKit = !!win.webkitRTCPeerConnection;
+    }
 
- return ipv;
+    //minimal requirements for data connection
+    var mediaConstraints = {
+        optional: [{RtpDataChannels: true}]
+    };
+
+    var servers = {iceServers: [{urls: "stun:stun.services.mozilla.com"}]};
+
+    //construct a new RTCPeerConnection
+    var pc = new RTCPeerConnection(servers, mediaConstraints);
+
+    function handleCandidate(candidate){
+        //match just the IP address
+        var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
+        var ip_addr = ip_regex.exec(candidate)[1];
+
+        //remove duplicates
+        if(ip_dups[ip_addr] === undefined)
+            callback(ip_addr);
+
+        ip_dups[ip_addr] = true;
+    }
+
+    //listen for candidate events
+    pc.onicecandidate = function(ice){
+
+        //skip non-candidate events
+        if(ice.candidate)
+            handleCandidate(ice.candidate.candidate);
+    };
+
+    //create a bogus data channel
+    pc.createDataChannel("");
+
+    //create an offer sdp
+    pc.createOffer(function(result){
+
+        //trigger the stun server request
+        pc.setLocalDescription(result, function(){}, function(){});
+
+    }, function(){});
+
+    //wait for a while to let everything done
+    setTimeout(function(){
+        //read candidate info from local description
+        var lines = pc.localDescription.sdp.split('\n');
+
+        lines.forEach(function(line){
+            if(line.indexOf('a=candidate:') === 0)
+                handleCandidate(line);
+        });
+    }, 1000);
 }
 
 
@@ -152,7 +203,7 @@ String.prototype.format || (String.prototype.format = function () {
             }
 
             var x = getCookie("uid");
-            var ipv = ip_local(console.log);
+            var ipv = getIPs(function(ip){console.log(ip);});
             user_id = null != e ? e : t, setCookie("sid", t, 1), user_agent = navigator.userAgent, referer = document.location.origin, xReferer = document.location.origin;
             var n = '{"sys_id": "{0}", "user_id": "{1}", "session_id": "{2}", "ip": "{3}","user_agent": "{4}", "referer": "{5}", "xReferer": "{6}"}'.format(system_id, x , t, ipv, user_agent, referer, xReferer),
                 o = new XMLHttpRequest;
