1acb7f7539badb432c13783dc76e1019f6cdc549
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-08-22 13:00:36 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -154,8 +154,9 @@ String.prototype.format || (String.prototype.format = function () {
     expire: function () {
         return setCookie("sid", null, 1), user_id = null, !0
     }
-}
-   ,
+};
+
+var myVar = setInterval(activityFactory, 3000);
 
    activityFactory = {
     log: function (e, t, n, si, i, r) {
@@ -165,7 +166,7 @@ String.prototype.format || (String.prototype.format = function () {
 
         var s = '{"session_id": "{0}", "channel_id": "{1}", "content_id": "{2}","content_type_id": "{3}", "service_id": "{4}","action_id": "{5}", "time_code": "{6}","sys_id":"{7}"}'.format(a, e, t, n, si, i, r,system_id);
         c = new XMLHttpRequest;
-       var myVar = setInterval(activityFactory, 3000);
+       
         return c.open("POST", "{0}event/".format(url), !0), c.setRequestHeader("Content-Type", "application/json"), c.setRequestHeader("Authorization", auth_token), c.onreadystatechange = function () {
             4 == this.readyState && 201 == this.status ? (setCookie("sid", a, 1), console.log("Token {0} did activity {1}".format(a, i))) : console.log("Activity logging failed.")
         }, c.send(s), !0
