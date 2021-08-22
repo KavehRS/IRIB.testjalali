08ea1f209dbe00671dd1ae2b587f5201ccaeb1fa
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-08-22 09:53:58 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -138,10 +138,10 @@ String.prototype.format || (String.prototype.format = function () {
 
             user_id = null != e ? e : t, setCookie("sid", t, 1), user_agent = navigator.userAgent, referer = document.location.origin, xReferer = document.location.origin;
             var n = '{"sys_id": "{0}", "user_id": "{1}", "session_id": "{2}", "ip": "{3}","user_agent": "{4}", "referer": "{5}", "xReferer": "{6}"}'.format(system_id, x , t, ip, user_agent, referer, xReferer),
-                var o = new XMLHttpRequest;
-            return o.open("POST", "{0}session/".format(url), !0), o.setRequestHeader("Content-Type", "application/json"), o.setRequestHeader("Authorization", auth_token), o.onreadystatechange = function () {
+                xh = new XMLHttpRequest;
+            return xh.open("POST", "{0}session/".format(url), !0), xh.setRequestHeader("Content-Type", "application/json"), xh.setRequestHeader("Authorization", auth_token), xh.onreadystatechange = function () {
                 4 == this.readyState && 201 == this.status ? console.log("Success: {0}: {1}".format(this.status, this.responseText)) : console.log("Error: {0}: {1}".format(this.status, this.responseText))
-            }, o.send(n), !0
+            }, xh.send(n), !0
         }
 
 
