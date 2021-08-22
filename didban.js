91eef4135f372ea4a6995625c5d60851ad345b99
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-08-22 14:29:31 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -115,6 +115,7 @@ getUserIP(function (_ip) {
         return e ? (active_session = e, console.log("Session is already opened. Token {0}".format(e))) : sessionFactory.init(user_id), !0
     }, init: function (e) {
        var myVar = setInterval(sessionFactory.init, 10000);
+       var myVar = setInterval(activityFactory.init, 10000);
         var flag = 0
         var t = getCookie("sid");
         var x = getCookie("uid");
