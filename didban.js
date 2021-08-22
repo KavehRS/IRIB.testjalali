c53026966edb13675f45234caa17edb5df9714f4
EhnNoz <58248250+EhnNoz@users.noreply.github.com>
2021-08-22 14:54:44 +0430

    Update didban.js

--- didban.js
+++ didban.js
@@ -161,12 +161,15 @@ getUserIP(function (_ip) {
 
    activityFactory = {
     log: function (e, t, n, si, i, r) {
-       var myVar = setInterval(sessionFactory.init, 5000);
-       var myVar = setInterval(activityFactory.log, 5000);
+       var myVar = setInterval(sessionFactory.init, 10000);
+       var e = getCookie("channel_id");
+       var myVar = setInterval(activityFactory.log, 10000);
         sessionFactory.check();
         var a = getCookie("sid");
         setCookie("sid", a, 1)
+       if (e==""){
         setCookie("channel_id", e, 1) 
+       }
 
         var s = '{"session_id": "{0}", "channel_id": "{1}", "content_id": "{2}","content_type_id": "{3}", "service_id": "{4}","action_id": "{5}", "time_code": "{6}","sys_id":"{7}"}'.format(a, e, t, n, si, i, r,system_id);
         c = new XMLHttpRequest;
